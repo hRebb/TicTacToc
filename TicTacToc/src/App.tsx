1352e70e84fbfa2bca5947a54b1ff0900895a32e
hamza_rebb <hamza.rebb@gmail.com>
2023-06-13 19:03:15 +0200

    start to dev about the app

--- TicTacToc/src/App.tsx
+++ TicTacToc/src/App.tsx
@@ -1,35 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react'
+import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0)
+type Player = 'X' | '0' | '';
+const initialBoard: Player[] = Array(16).fill('');
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+const App = () => {
+  const [board, setBoard] = useState<Player[]>(initialBoard);
+  const [currentPlayer, setCurrentPlayer] = useState<Player>('X');
+  const [winner, setWinner] = useState<Player>('');
+
+  const handleCellClick = (index: number) => {
+    if (board[index] !== '' || winner !== '') {
+      return;
+    }
+
+    const newBoard = [...board];
+    newBoard[index] = currentPlayer;
+    setBoard(newBoard);
+  }
 }
-
-export default App
